075143e4628a55c983ca63ab75d1f0410db02208
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2025-03-06 13:13:33 +0600

    initial commit🚀

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+<template>
+    <div class="grid">
+        <div class="col-12 lg:col-6 xl:col-3">
+            <div class="card mb-0">
+                <NuxtLink to="companies" class="flex justify-content-between mb-3">
+                    <div>
+                        <span class="block text-500 font-medium mb-3">HRS</span>
+                        <div class="text-900 font-medium text-xl">1</div>
+                    </div>
+                    <div class="flex align-items-center justify-content-center bg-blue-100 border-round" style="width: 2.5rem; height: 2.5rem">
+                        <i class="pi pi-microsoft text-blue-500 text-xl"></i>
+                    </div>
+                </NuxtLink>
+                <!-- <span class="text-green-500 font-medium">24 new </span> -->
+                <!-- <span class="text-500">since last visit</span> -->
+            </div>
+        </div>
+    </div>
+</template>
